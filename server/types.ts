ac2b475dbfda4538bfb25ff20b8de76abb64dc34
Caleb Wood <calebw787@gmail.com>
2023-10-29 16:47:24 -0700

    Adds types file

---
+++ server/types.ts
@@ -0,0 +1,33 @@
+export interface VoterMessage {
+  userId: string;
+  fingerprint: VoterFingerprint;
+  vote: Vote;
+}
+
+export interface VoterFingerprint {
+  userAgent: string;
+  screenSize: string;
+  language: string;
+}
+
+export interface Vote {
+  id: string;
+  userId: string;
+  framework: FrameworkTitle;
+  created_at: string;
+  voteHistory: string;
+}
+
+export type FrameworkTitle =
+  "react" |
+  "solid" |
+  "svelte" |
+  "marko" |
+  "htmx" |
+  "astro" |
+  "vue" |
+  "angular" |
+  "alpine" |
+  "preact" |
+  "quik" |
+  "lit";
